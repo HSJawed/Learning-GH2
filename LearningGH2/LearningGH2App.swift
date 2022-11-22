61281588251b9758fa697631df10d316e4c8ee1a
Jawed <hsjawed@yahoo.com>
2022-11-22 10:31:24 +0600

    Initial Commit

---
+++ LearningGH2/LearningGH2App.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGH2App.swift
+//  LearningGH2
+//
+//  Created by MD HUSSAIN SHAH JAWED on 22/11/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningGH2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
